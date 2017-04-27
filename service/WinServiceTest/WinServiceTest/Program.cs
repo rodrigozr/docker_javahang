af1d677563649cafa172a8d4d7545b9099e612d9
rrosauro <rrosauro@rdisoftware.com>
2017-04-27 18:44:10 -0300

    Added Windows Service test - Per Microsoft suggestion (no luck...)

---
+++ service/WinServiceTest/WinServiceTest/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace WinServiceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<Program>(s =>
+                {
+                    s.ConstructUsing(tc => new Program());
+                    s.WhenStarted(tc =>
+                    {
+                        var dir = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+                        using (var log = new StreamWriter(File.OpenWrite(Path.Combine(dir, "log.txt"))))
+                        {
+                            Run(log);
+                        }
+                    });
+                    s.WhenStopped(tc => { });
+                });
+
+                x.RunAsLocalSystem();
+                x.SetDescription("Java hang test");
+                x.SetDisplayName("Java hang test");
+                x.SetServiceName("JavaHangTest");
+            });
+        }
+
+        private static void Run(StreamWriter log)
+        {
+            log.WriteLine("Starting...");
+            try
+            {
+                var psi = new ProcessStartInfo()
+                {
+                    WorkingDirectory = @"c:\workspace\docker_javahang",
+                    FileName = @"c:\j2sdk1.4.2_19\jre\bin\java.exe",
+                    Arguments = "HelloWorld",
+                };
+                var process = new Process() {StartInfo = psi};
+                var res = process.Start();
+                log.WriteLine("Running java. Result:" + res);
+            }
+            catch (Exception e)
+            {
+                log.WriteLine(e);
+            }
+        }
+    }
+}
